3e2937b4934e88c020d0c52d5d8330d2c132653c
Ahmed Helmy <115832929+5elmy@users.noreply.github.com>
2024-11-27 23:32:49 +0200

    @#12345

--- src/app/hooks/postDetails.hook.ts
+++ src/app/hooks/postDetails.hook.ts
@@ -8,11 +8,11 @@ export default async function PostDetailsHook({postId}:{postId:string | number})
 
 
   
-     let [postsResponse, commentsResponse] = await Promise.all([
+     const [postsResponse, commentsResponse] = await Promise.all([
        fetch(`${BASEURL}/posts/${postId}`),
        fetch(`${BASEURL}/comments?postId=${postId}`),
      ]);
-     let [post, comments] = await Promise.all([
+     const [post, comments] = await Promise.all([
        postsResponse.json(),
        commentsResponse.json(),
      ]);
